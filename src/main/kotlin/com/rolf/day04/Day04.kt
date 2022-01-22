24edf7ceca9089ae8da200fddfcf7eecfd712145
Rolf <rolfschellenberger@gmail.com>
2022-01-23 00:31:10 +0100

    Day 4

--- src/main/kotlin/com/rolf/day04/Day04.kt
+++ src/main/kotlin/com/rolf/day04/Day04.kt
@@ -1,6 +1,7 @@
 package com.rolf.day04
 
 import com.rolf.Day
+import com.rolf.util.getCharacterCounts
 
 fun main() {
     Day04().run()
@@ -8,10 +9,36 @@ fun main() {
 
 class Day04 : Day() {
     override fun solve1(lines: List<String>) {
-        TODO("Not yet implemented")
+        val (from, to) = lines.first().split("-").map { it.toInt() }
+        val options = (from..to)
+            .filter { adjacentNumbers(it) }
+            .filter { increases(it) }
+            .count()
+        println(options)
     }
 
     override fun solve2(lines: List<String>) {
-        TODO("Not yet implemented")
+        val (from, to) = lines.first().split("-").map { it.toInt() }
+        val options = (from..to)
+            .filter { adjacentNumbers2(it) }
+            .filter { increases(it) }
+            .count()
+        println(options)
+    }
+
+    private fun adjacentNumbers(number: Int): Boolean {
+        return getCharacterCounts(number.toString())
+            .filter { it.key >= 2 }
+            .count() > 0
+    }
+
+    private fun increases(number: Int): Boolean {
+        return number.toString() == number.toString().toCharArray().sorted().joinToString("")
+    }
+
+    private fun adjacentNumbers2(number: Int): Boolean {
+        return getCharacterCounts(number.toString())
+            .filter { it.key == 2 }
+            .count() > 0
     }
 }
