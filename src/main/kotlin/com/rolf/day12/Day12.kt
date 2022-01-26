714933fe058de9aeff4def2b7c7d2235edc316c1
Rolf Schellenberger <rschellenberger@bol.com>
2022-01-26 14:05:46 +0100

    Day 12

--- src/main/kotlin/com/rolf/day12/Day12.kt
+++ src/main/kotlin/com/rolf/day12/Day12.kt
@@ -2,6 +2,7 @@ package com.rolf.day12
 
 import com.rolf.Day
 import com.rolf.util.Location
+import com.rolf.util.leastCommonMultiple
 import kotlin.math.absoluteValue
 
 fun main() {
@@ -26,18 +27,41 @@ class Day12 : Day() {
     override fun solve2(lines: List<String>) {
         val moons = lines.map { parseMoon(it) }
 
-        val cache = mutableSetOf<Moon>(moons.first())
-        for (i in 0 until 10000) {
+        // Find the cycle of x, y and z to find the least common multiple for the final step where they 'align' for
+        // the first time.
+        val firstX: List<Pair<Int, Int>> = moons.map { it.position.x to it.velocity.x }
+        val firstY: List<Pair<Int, Int>> = moons.map { it.position.y to it.velocity.y }
+        val firstZ: List<Pair<Int, Int>> = moons.map { it.position.z to it.velocity.z }
+        var stepX = 0
+        var stepY = 0
+        var stepZ = 0
+        var step = 0
+        while (stepX == 0 || stepY == 0 || stepZ == 0) {
+            step++
             moons.forEach {
                 it.updateVelocity(moons)
             }
             moons.forEach {
                 it.updatePosition()
             }
-            if (!cache.add(moons.first())) {
-                println("Found duplicate: $i = ${moons.first()}")
-            }
+            stepX = checkIfEqualState(stepX, moons.map { it.position.x to it.velocity.x }, firstX, step)
+            stepY = checkIfEqualState(stepY, moons.map { it.position.y to it.velocity.y }, firstY, step)
+            stepZ = checkIfEqualState(stepZ, moons.map { it.position.z to it.velocity.z }, firstZ, step)
         }
+
+        println(leastCommonMultiple(stepX.toLong(), leastCommonMultiple(stepY.toLong(), stepZ.toLong())))
+    }
+
+    private fun checkIfEqualState(
+        stepValue: Int,
+        state: List<Pair<Int, Int>>,
+        first: List<Pair<Int, Int>>,
+        step: Int
+    ): Int {
+        if (stepValue == 0 && state == first) {
+            return step
+        }
+        return stepValue
     }
 
     private fun parseMoon(line: String): Moon {
