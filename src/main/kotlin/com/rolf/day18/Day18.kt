7dca05a5ab9c14a51137ff14120cb52aa5ee11db
Rolf <rolfschellenberger@gmail.com>
2022-12-26 18:14:31 +0100

    Day 18

--- src/main/kotlin/com/rolf/day18/Day18.kt
+++ src/main/kotlin/com/rolf/day18/Day18.kt
@@ -1,124 +1,131 @@
 package com.rolf.day18
 
 import com.rolf.Day
+import com.rolf.util.*
 
 fun main() {
     Day18().run()
 }
 
+typealias Position = Pair<String, Point>
+
 class Day18 : Day() {
     override fun solve1(lines: List<String>) {
-        val maze = Maze.from(lines)
-        println(maze.minimumSteps())
+//        val grid = MatrixString.build(splitLines(lines))
+//        println(grid)
+        // Make a graph from each location to each location
+        // b.A.@.a ->
+        // @ - a
+        //  \
+        //   A - b
+
+        // ########################
+        // #f.D.E.e.C.b.A.@.a.B.c.#
+        // ######################.#
+        // #d.....................#
+        // ########################
+        // @ > a > b > c > d > e > f
+        // @ > a > b > c > e > d > f
+
+        // #################
+        // #i.G..c...e..H.p#
+        // ########.########
+        // #j.A..b...f..D.o#
+        // ########@########
+        // #k.E..a...g..B.n#
+        // ########.########
+        // #l.F..d...h..C.m#
+        // #################
+        // @ - c - G - i
+        //   - e - H - p
+        //   - b - A - j
+        //   - f - D - o
+        //   - a - E - k
+        //   - g - B - n
+        //   - d - F - l
+        //   - h - C - m
+        // @ -
+
+        // Make graph with all keys + starting position being the vertices
+        // Remove the starting position from the map and set current position to starting position (@)
+
+        // 1. Find all keys on the map
+        //    When there are no more keys on the map, stop the iteration
+        // 2. See what keys are reachable from the current position
+        //    When there are no more keys reachable, stop the iteration
+        // 3. For each key reachable, remove the key + door and move the position
+        // 4. Add the vertex from current position to key position with the distance as the weight
+        // 5. Go to step 1
+
+        // Find the shortest path from the starting position through all edges
+
+        // Make graph with all keys + starting position being the vertices
+        val graph = Graph<String>()
+        val grid = MatrixString.build(splitLines(lines))
+        val start = grid.find("@").first()
+        val keys = getKeysOnGrid(grid)
+        keys.map { it.first }
+            .forEach { graph.addVertex(Vertex(it)) }
+        graph.addVertex(Vertex("@"))
+        println(graph.vertices())
+        println(grid)
+
+        travel(graph, start, grid.copy())
+
+        // Find the shortest path from the starting position through all edges
+        println(graph.vertices())
+        println(graph.edges())
+        val result = graph.lowestPathAndWeightVisitAll("@")
+        println(result)
+        println(result.second.toInt())
+    }
+
+    private fun travel(graph: Graph<String>, position: Point, grid: MatrixString) {
+        // Remove the position and the door
+        val value = grid.get(position)
+        grid.replace(
+            mapOf(
+                value to ".",
+                value.uppercase() to "."
+            )
+        )
+        println(grid)
+
+        // 1. Find all remaining keys on the map
+        //    When there are no more keys on the map, stop the iteration
+        val keys = getKeysOnGrid(grid)
+        val notAllowedValues = keys.map { it.first.uppercase() }.toSet() + "#"
+        if (keys.isNotEmpty()) {
+
+            // 2. See what keys are reachable from the current position
+            //    When there are no more keys reachable, stop the iteration
+            for (key in keys) {
+                val paths = grid.findPathsByValue(position, setOf(key.second), notAllowedValues, diagonal = false)
+                println(key)
+                println(paths)
+                // 3. For each key reachable, remove the key + door and move the position
+                if (paths.isNotEmpty()) {
+                    // 4. Add the edge from current position to key position with the distance as the weight
+                    graph.addEdge(value, key.first, EdgeType.DIRECTED, paths.minOf { it.size }.toDouble())
+                    // 5. Go to step 1
+                    travel(graph, key.second, grid.copy())
+                }
+            }
+        }
+    }
+
+    private fun getKeysOnGrid(grid: MatrixString): List<Position> {
+        return grid.allPoints()
+            .map { Position(grid.get(it), it) }
+            .filter { it.first.first() in 'a'..'z' }
+    }
+
+    private fun getDoorsOnGrid(grid: MatrixString): List<Position> {
+        return grid.allPoints()
+            .map { Position(grid.get(it), it) }
+            .filter { it.first.first() in 'A'..'Z' }
     }
 
     override fun solve2(lines: List<String>) {
-        val maze = Maze.from(lines)
-        maze.openSpaces.remove(Point2D(39, 39))
-        maze.openSpaces.remove(Point2D(40, 39))
-        maze.openSpaces.remove(Point2D(41, 39))
-        maze.openSpaces.remove(Point2D(39, 40))
-        maze.openSpaces.remove(Point2D(41, 40))
-        maze.openSpaces.remove(Point2D(39, 41))
-        maze.openSpaces.remove(Point2D(40, 41))
-        maze.openSpaces.remove(Point2D(41, 41))
-
-        maze.starts.remove(Point2D(40, 40))
-        maze.starts.add(Point2D(39, 39))
-        maze.starts.add(Point2D(41, 39))
-        maze.starts.add(Point2D(39, 41))
-        maze.starts.add(Point2D(41, 41))
-
-        println(maze.minimumSteps())
     }
-}
-
-class Maze(
-    val starts: MutableSet<Point2D>,
-    private val keys: Map<Point2D, Char>,
-    private val doors: Map<Point2D, Char>,
-    val openSpaces: MutableSet<Point2D>
-) {
-
-    private fun findReachableKeys(from: Point2D, haveKeys: Set<Char> = mutableSetOf()): Map<Char, Pair<Point2D, Int>> {
-        val queue = ArrayDeque<Point2D>().apply { add(from) }
-        val distance = mutableMapOf(from to 0)
-        val keyDistance = mutableMapOf<Char, Pair<Point2D, Int>>()
-        while (queue.isNotEmpty()) {
-            val next = queue.removeFirst()
-            next.neighbors()
-                .filter { it in openSpaces }
-                .filterNot { it in distance }
-                .forEach { point ->
-                    distance[point] = distance[next]!! + 1
-                    val door = doors[point]
-                    val key = keys[point]
-                    if (door == null || door.lowercaseChar() in haveKeys) {
-                        if (key != null && key !in haveKeys) {
-                            keyDistance[key] = Pair(point, distance[point]!!)
-                        } else {
-                            queue.add(point)
-                        }
-                    }
-                }
-        }
-        return keyDistance
-    }
-
-    private fun findReachableFromPoints(
-        from: Set<Point2D>,
-        haveKeys: Set<Char>
-    ): Map<Char, Triple<Point2D, Int, Point2D>> =
-        from.map { point ->
-            findReachableKeys(point, haveKeys).map { entry ->
-                entry.key to Triple(entry.value.first, entry.value.second, point)
-            }
-        }.flatten().toMap()
-
-    fun minimumSteps(
-        from: Set<Point2D> = starts,
-        haveKeys: Set<Char> = mutableSetOf(),
-        seen: MutableMap<Pair<Set<Point2D>, Set<Char>>, Int> = mutableMapOf()
-    ): Int {
-        val state = Pair(from, haveKeys)
-
-        if (state in seen) return seen.getValue(state)
-
-        val answer = findReachableFromPoints(from, haveKeys).map { entry ->
-            val (at, dist, cause) = entry.value
-            dist + minimumSteps((from - cause) + at, haveKeys + entry.key, seen)
-        }.minOrNull() ?: 0
-        seen[state] = answer
-        return answer
-    }
-
-    companion object {
-        fun from(input: List<String>): Maze {
-            val starts = mutableSetOf<Point2D>()
-            val keys = mutableMapOf<Point2D, Char>()
-            val doors = mutableMapOf<Point2D, Char>()
-            val openSpaces = mutableSetOf<Point2D>()
-
-            input.forEachIndexed { y, row ->
-                row.forEachIndexed { x, c ->
-                    val place = Point2D(x, y)
-                    if (c == '@') starts += place
-                    if (c != '#') openSpaces += place
-                    if (c in ('a'..'z')) keys[place] = c
-                    if (c in ('A'..'Z')) doors[place] = c
-                }
-            }
-            return Maze(starts, keys, doors, openSpaces)
-        }
-    }
-}
-
-data class Point2D(val x: Int, val y: Int) {
-    fun up(): Point2D = copy(y = y + 1)
-    fun down(): Point2D = copy(y = y - 1)
-    fun left(): Point2D = copy(x = x - 1)
-    fun right(): Point2D = copy(x = x + 1)
-
-    fun neighbors(): List<Point2D> =
-        listOf(up(), down(), left(), right())
 }
