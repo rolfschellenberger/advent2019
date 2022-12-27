6146ecfa5eed3be73602c36aea962eafd575a67a
Rolf <rolfschellenberger@gmail.com>
2022-12-27 21:26:18 +0100

    Day 18

--- src/main/kotlin/com/rolf/day18/Day18.kt
+++ src/main/kotlin/com/rolf/day18/Day18.kt
@@ -3,6 +3,7 @@ package com.rolf.day18
 import com.rolf.Day
 import com.rolf.util.MatrixString
 import com.rolf.util.Point
+import com.rolf.util.getPermutations
 import com.rolf.util.splitLines
 
 fun main() {
@@ -16,40 +17,11 @@ class Day18 : Day() {
         val location = grid.find("@").first()
         val keys = getKeysOnGrid(grid)
         val doors = getDoorsOnGrid(grid)
-        println(travel(grid, State(location, keys, doors)))
-    }
 
-    private fun travel(
-        grid: MatrixString,
-        state: State,
-        cache: MutableMap<State, Int> = mutableMapOf()
-    ): Int {
-        // When all keys are retrieved, return the distance traveled
-        if (state.keys.isEmpty()) {
-            return 0
-        }
-        println(state)
+        // Pre-compute paths from start or key to another key, returning the length + the required keys needed to get there
+        preComputePaths(grid, location, keys, doors)
 
-        if (cache.containsKey(state)) {
-            return cache.getValue(state)
-        }
-
-        val defaultBoard = buildDefaultBoard(grid, state.doors)
-        // Travel to each key
-        var minDistance = Int.MAX_VALUE
-        for (key in state.keys) {
-            val path = defaultBoard.findPathByValue(state.location, key.point, setOf("#"))
-            if (path.isNotEmpty()) {
-                // When the key can be reached, open its door
-                val doors = openDoor(state.doors, key)
-                val newState = State(key.point, state.keys - key, doors)
-                val distance = path.size + travel(grid, newState, cache)
-                minDistance = minOf(minDistance, distance)
-            }
-        }
-
-        cache[state] = minDistance
-        return minDistance
+        println(travel(State(location, emptySet()), keys))
     }
 
     private fun getKeysOnGrid(grid: MatrixString): Set<Position> {
@@ -66,6 +38,23 @@ class Day18 : Day() {
             .toSet()
     }
 
+    // Map with Pair(from -> to) to Pair(requiredKeys, distance)
+    private val memory: MutableMap<Pair<Point, Point>, Pair<Set<String>, Int>> = mutableMapOf()
+
+    private fun preComputePaths(grid: MatrixString, location: Point, keys: Set<Position>, doors: Set<Position>) {
+        val defaultBoard = buildDefaultBoard(grid, emptySet())
+
+        val list = keys.map { it.point } + location
+        val permutations = getPermutations(list, 2)
+        for ((from, to) in permutations) {
+            val path = defaultBoard.findPathByValue(from, to, setOf("#"))
+            // Find doors on path
+            val doorsOnPath = doors.filter { path.contains(it.point) }
+            val requiredKeys = doorsOnPath.map { it.value.lowercase() }.toSet()
+            memory[from to to] = requiredKeys to path.size
+        }
+    }
+
     private fun buildDefaultBoard(
         grid: MatrixString,
         doors: Set<Position>
@@ -76,20 +65,75 @@ class Day18 : Day() {
         return defaultBoard
     }
 
-    private fun openDoor(doors: Set<Position>, key: Position): Set<Position> {
-        return doors.filterNot {
-            it.value == key.value.uppercase()
-        }.toSet()
+    private fun getPathLength(from: Point, to: Point): Int {
+        return memory.getValue(from to to).second
+    }
+
+    private fun canTravelTo(from: Point, to: Point, keys: Set<Position>): Boolean {
+        val keysNeeded = memory.getValue(from to to).first
+        return keys.map { it.value }.containsAll(keysNeeded)
+    }
+
+    private fun travel(
+        state: State,
+        keysToCollect: Set<Position>,
+        cache: MutableMap<State, Int> = mutableMapOf()
+    ): Int {
+        // When all keys are retrieved, return the distance traveled
+        if (state.keys.size == keysToCollect.size) {
+            return 0
+        }
+
+        if (cache.containsKey(state)) {
+            return cache.getValue(state)
+        }
+
+        // Travel to each key
+        var minDistance = Int.MAX_VALUE
+        val from = state.location
+        for (key in keysToCollect - state.keys) {
+            // Can we travel to this location with our current key set?
+            val to = key.point
+            val collectedKeys = state.keys
+            if (canTravelTo(from, to, collectedKeys)) {
+                // When the key can be reached, open its door
+                val newState = State(key.point, state.keys + key)
+                val distance = getPathLength(from, key.point) + travel(newState, keysToCollect, cache)
+                minDistance = minOf(minDistance, distance)
+            }
+        }
+
+        cache[state] = minDistance
+        return minDistance
     }
 
     override fun solve2(lines: List<String>) {
+        val grid = MatrixString.build(splitLines(lines))
+        val location = grid.find("@").first()
+        grid.set(grid.getLeftUp(location)!!, "@")
+        grid.set(grid.getRightUp(location)!!, "@")
+        grid.set(grid.getLeftDown(location)!!, "@")
+        grid.set(grid.getRightDown(location)!!, "@")
+        grid.set(location, "#")
+        grid.set(grid.getUp(location)!!, "#")
+        grid.set(grid.getDown(location)!!, "#")
+        grid.set(grid.getLeft(location)!!, "#")
+        grid.set(grid.getRight(location)!!, "#")
+        println(grid)
+        val keys = getKeysOnGrid(grid)
+        val doors = getDoorsOnGrid(grid)
+
+        // Pre-compute paths from start or key to another key, returning the length + the required keys needed to get there
+        preComputePaths(grid, location, keys, doors)
+
+        println(travel(State(location, emptySet()), keys))
     }
 }
 
 data class Position(val value: String, val point: Point)
 
-data class State(val location: Point, val keys: Set<Position>, val doors: Set<Position>) {
+data class State(val location: Point, val keys: Set<Position>) {
     override fun toString(): String {
-        return "State(location=$location, keys=${keys.size}, doors=${doors.size})"
+        return "State(location=$location, keys=${keys.size})"
     }
 }
