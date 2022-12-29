2c29b6f042d20f33a0cc0f3c0a49b8dff4a3600b
Rolf <rolfschellenberger@gmail.com>
2022-12-29 23:20:56 +0100

    Day 24

--- src/main/kotlin/com/rolf/day24/Day24.kt
+++ src/main/kotlin/com/rolf/day24/Day24.kt
@@ -1,6 +1,9 @@
 package com.rolf.day24
 
 import com.rolf.Day
+import com.rolf.util.MatrixString
+import com.rolf.util.splitLines
+import kotlin.math.pow
 
 fun main() {
     Day24().run()
@@ -8,10 +11,33 @@ fun main() {
 
 class Day24 : Day() {
     override fun solve1(lines: List<String>) {
-        TODO("Not yet implemented")
+        val grid = MatrixString.build(splitLines(lines))
+
+        val unique = mutableSetOf(grid.toString().hashCode())
+        while (true) {
+            val copy = grid.copy()
+            for (point in copy.allPoints()) {
+                val neighbours = copy.getNeighbours(point, diagonal = false)
+                val bugCount = neighbours.map { copy.get(it) }.count { it == "#" }
+                val value = when (copy.get(point)) {
+                    "#" -> if (bugCount == 1) "#" else "."
+                    else -> if (bugCount in 1..2) "#" else "."
+                }
+                grid.set(point, value)
+            }
+            if (!unique.add(grid.toString().hashCode())) {
+                var sum = 0L
+                for ((index, point) in grid.allPoints().withIndex()) {
+                    if (grid.get(point) == "#") {
+                        sum += 2.0.pow(index).toLong()
+                    }
+                }
+                println(sum)
+                break
+            }
+        }
     }
 
     override fun solve2(lines: List<String>) {
-        TODO("Not yet implemented")
     }
 }
